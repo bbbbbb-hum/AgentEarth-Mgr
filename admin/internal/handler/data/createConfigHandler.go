59c2f6e39d021f5b5d42199e59c67d19899f64d4
zhang <zhangshoubao-SGiuK>
2026-01-08 12:07:49 +0800

    Complete API code update with new fields

--- admin/internal/handler/data/createConfigHandler.go
+++ admin/internal/handler/data/createConfigHandler.go
@@ -8,13 +8,20 @@ import (
 
 	"AgentEarth-Mgr/admin/internal/logic/data"
 	"AgentEarth-Mgr/admin/internal/svc"
+	"AgentEarth-Mgr/admin/internal/types"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func CreateConfigHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.CreateConfigReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
 		l := data.NewCreateConfigLogic(r.Context(), svcCtx)
-		resp, err := l.CreateConfig()
+		resp, err := l.CreateConfig(&req)
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
--- admin/internal/handler/data/updateServiceDescHandler.go
+++ admin/internal/handler/data/updateServiceDescHandler.go
@@ -8,13 +8,20 @@ import (
 
 	"AgentEarth-Mgr/admin/internal/logic/data"
 	"AgentEarth-Mgr/admin/internal/svc"
+	"AgentEarth-Mgr/admin/internal/types"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func UpdateServiceDescHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UpdateServiceDescReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
 		l := data.NewUpdateServiceDescLogic(r.Context(), svcCtx)
-		resp, err := l.UpdateServiceDesc()
+		resp, err := l.UpdateServiceDesc(&req)
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
