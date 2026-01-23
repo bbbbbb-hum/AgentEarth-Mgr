daf67a337e8cf41b4fbd74cf06950db0ea60bc17
Chen Boliang <Chen120072.>
2026-01-23 14:48:12 +0800

    fix: 修复在管理员修改价格时记录应为username

--- models/users/aemcpexternalservicesusermodel.go
+++ models/users/aemcpexternalservicesusermodel.go
@@ -37,7 +37,7 @@ func (m *customAeMcpExternalServicesUserModel) withSession(session sqlx.Session)
 
 func (m *customAeMcpExternalServicesUserModel) FindOneByUserId(ctx context.Context, userId string) (*AeMcpExternalServicesUser, error) {
 	var resp AeMcpExternalServicesUser
-	query := fmt.Sprintf("select %s from %s where user_id = $1 limit 1", aeMcpExternalServicesUserRows, m.table)
+	query := fmt.Sprintf("select %s from %s where user_str_id = $1 limit 1", aeMcpExternalServicesUserRows, m.table)
 	err := m.conn.QueryRowCtx(ctx, &resp, query, userId)
 	switch err {
 	case nil:
